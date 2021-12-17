b0a2ee172b3863a71c231ccab5924b7393811d32
Shannon Schrauwen <63632791+ShanSchrauwen@users.noreply.github.com>
2021-12-17 21:33:11 +0200

    Full stack exercises version 1

---
+++ FullStackExercises_1_Schrauwen/src/main/webapp/studentList.js
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+function main() {
+	fetch("http://localhost:8080/WebAppExercises/students")
+		.then(response => response.json())
+		.then(studentList => printStudents(studentList))
+}
+
+function printStudents(studentList) {
+	var tableOutput = document.getElementById("tableBody")
+
+	var tableData = "<tr>" +
+		"<th> ID </th>" +
+		"<th> Last name </th>" +
+		"<th> First name </th>" +
+		"<th> Street </th>" +
+		"<th> Postcode </th>" +
+		"<th> Post office </th>"
+
+
+	console.log(studentList)
+
+	for (var student of studentList) {
+		tableData += "<tr> <td>"
+			+ student.id
+			+ "</td> <td>"
+			+ student.lastName
+			+ "</td> <td>"
+			+ student.firstName
+			+ "</td> <td>"
+			+ student.streetAddress
+			+ "</td> <td>"
+			+ student.postCode
+			+ "</td> <td>"
+			+ student.postOffice
+			+ "</td> </tr>"
+	}
+
+	tableOutput.innerHTML = tableData
+}
+
+main();
